e250db41258b98570593edcfc88c3ce6e3df7660
Nykolas Catalão <nykolas.catalao@estudante.ufjf.br>
2025-08-05 12:53:45 -0300

    Pequenas mudanças no estilo e desenvolvimento da lógica de movimentação e de conclusão do jogo

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const tabuleiro = document.querySelector("#tabuleiro");
 const mensagem = document.querySelector("#mensagem");
 
 // Inicialização do tabuleiro 
+
 const posicaoInicial = ["preta","preta","preta", "","branca","branca","branca"];
 let posicao = structuredClone(posicaoInicial); // Cópia da posição inicial do tabuleiro
 
@@ -19,9 +20,40 @@ function renderizarTabuleiro() {
             casa.appendChild(peca);
         }
 
-        
+        casa.addEventListener("click", () => moverPeca(i));
         tabuleiro.appendChild(casa);
     };
+}
+
+function moverPeca(i) {
+    const vazio = posicao.indexOf("");
+    const distancia = Math.abs(vazio - i);
+    // Se a casa escolhida possuir uma peça e o movimento obedecer às regras
+    if((distancia === 1 || distancia === 2) && posicao[i] !== "") {
+        mensagem.textContent = "";
+        mensagem.classList.remove("ok");
+
+        let aux = posicao[i];
+        posicao[i] = posicao[vazio];
+        posicao[vazio] = aux;
+
+        renderizarTabuleiro();
+
+        //Confere se todas as peças estão no lugar correto para determinar o fim do jogo
+        if (fimDoJogo()) {
+            mensagem.textContent = "Fim de jogo! Todas as peças estão no local correto. Parabéns!"
+            mensagem.classList.add("ok");
+            casa.removeEventListener("click", moverPeca);
+        }
+    } else {
+        mensagem.textContent = "Movimento inválido!";
+        mensagem.classList.remove("ok");
+    }
+}
+
+function fimDoJogo() {
+    let verificador = JSON.stringify(posicao) === JSON.stringify(["branca","branca","branca", "", "preta","preta","preta"]);
+    return verificador;
 }
 
 renderizarTabuleiro();
